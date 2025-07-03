fbb50b61c602d200b2417712dbaaaada9655f55d
Armaan <armaansinghbhau8@gmail.com>
2025-07-03 10:47:39 +0530

    recrusion concepts

---
+++ Day_3/recursion_1.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int num = 0;
+
+void func(){
+    if(num == 5)
+        return;
+
+        num++;
+        func();
+
+        cout << "Hello World" << endl;
+}
+
+int main(){
+    func();
+    return 0;
+}
