239dfd12f13d468c946b442a8267a1ee24c9dfa4
Armaan <armaansinghbhau8@gmail.com>
2025-07-05 15:31:23 +0530

    structure pointer

---
+++ Day_4/structure_pointer.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<stdio.h>
+#include<stdlib.h>
+using namespace std;
+
+struct Data{
+    int a;
+    float b;
+};
+
+struct Data *p;
+
+int main(){
+    // int *p;
+    //p = malloc(sizeof(int))
+
+    p = (struct Data*)malloc(sizeof(struct Data));
+    (*p).a = 10;
+    p -> b = 12.4;
+    cout << (*p).a << "\t" << p -> b << endl;
+    free(p);
+    return 0;
+}
