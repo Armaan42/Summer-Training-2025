5d7649f1951fba4d934f1bdc04b67071ee57cd28
Armaan <armaansinghbhau8@gmail.com>
2025-07-08 11:20:00 +0530

    update

---
+++ Day_6/constructor_2.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+
+class party{
+    public:
+        int samosa;
+        int methai;
+        string drink;
+
+        // initializer list
+        party(): samosa(2), methai(3), drink("Cold Coffee"){}
+    
+    // party(){
+    //     samosa = 2;
+    //     methai = 3;
+    //     drink = "cold coffee";
+    // }
+
+    void print(){
+        cout << "Samosa: " << samosa << endl;
+        cout << "Sweets: " << methai << endl;
+        cout << "Drink: " << drink << endl;
+    }
+};
+
+int main(){
+    party p1;
+    p1.print();
+    return 0;
+}
