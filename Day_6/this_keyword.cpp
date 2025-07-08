9aad5405b674e7a53f1b41eb9a60551bf80a516a
Armaan <armaansinghbhau8@gmail.com>
2025-07-08 13:13:47 +0530

    this keyword

---
+++ Day_6/this_keyword.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+class btech{
+    public:
+        int roll;
+        float cgpa;
+
+        btech(): roll(555), cgpa(8.8){}
+
+        void print(){
+            cout << "Address using this keyword: " << this << endl;
+        }
+};
+
+int main(){
+    btech b1;
+    cout << "Address of b1: " << &b1 << endl;
+    b1.print();
+
+}
