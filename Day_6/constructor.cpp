b76463dd6a74416bc01890c75a16014ec12b92b4
Armaan <armaansinghbhau8@gmail.com>
2025-07-08 10:29:18 +0530

    constructor type

---
+++ Day_6/constructor.cpp
@@ -0,0 +1,37 @@
+/*
+Constructor: constuctor are the function define inside class and function name must be same as class name.
+    - constructor did not have any return type.
+    - we can not call constructor function, it was invoke when object of a class is created.
+    - constructor can be override.
+
+Types of constructor:
+    1. Default constructor (or Non-parameterised constructor):
+        - Constructor did not have any parameter is called default constructor.
+
+    2. Parameterised constructor:
+        - Constructor having parameter is called parametersized constructor.
+
+    3. copy constructor:
+        - A constructor that initialized an object using another object of the same class.
+*/
+
+#include<iostream>
+using namespace std;
+
+class btech{
+    public:
+        int roll;
+        float cgpa;
+
+        btech(){
+            cout << "I am default constructor" << endl;
+        }
+};
+
+int main(){
+    btech n1, n2, n3, n4;       // static allocation
+    btech *s = new btech();     // Dynamic allocation
+
+
+    return 0;
+}
